4131e24881ad6e4794431b48b808640378083fda
Simon Théorêt <simonteoret@hotmail.com>
2025-03-07 22:43:48 -0500

    Updating the flake, aws-config deps

--- flake.nix
+++ flake.nix
@@ -7,18 +7,32 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, rust-overlay, flake-utils, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
+  outputs =
+    {
+      self,
+      nixpkgs,
+      rust-overlay,
+      flake-utils,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
       let
         overlays = [ (import rust-overlay) ];
         pkgs = import nixpkgs { inherit system overlays; };
         rust = pkgs.rust-bin.stable.latest.default.override {
-          extensions = [ "rust-src" ];
+          extensions = [
+            "rust-src"
+            "rust-analyzer"
+            "rustfmt"
+          ];
           targets = [ ];
         };
-      in {
+      in
+      {
         devShells.default = pkgs.mkShell {
-          buildInputs = [ rust ] ++ (with pkgs; [ rust-analyzer ]);
+          buildInputs = [ rust ] ++ (with pkgs; [ ]);
         };
-      });
+      }
+    );
 }
